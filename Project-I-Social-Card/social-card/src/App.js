ab18c5a5347c4749b1e8351e0560fb4f4ebac2e6
ills5771 <ilyayelizarov1@gmail.com>
2019-02-11 12:50:05 -0800

    changed width and margin in App.css

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a

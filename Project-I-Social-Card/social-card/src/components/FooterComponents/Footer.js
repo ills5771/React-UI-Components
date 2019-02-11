a8f04f79b6b0a7d23070694fa34d48ecea4d9c68
ills5771 <ilyayelizarov1@gmail.com>
2019-02-11 14:42:15 -0800

    Created Footer component, with mock text for now

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>comment</p>
+      <p>retweet</p>
+      <p>like</p>
+      <p>email</p>
+    </div>
+  );
+};
+
+export default Footer;

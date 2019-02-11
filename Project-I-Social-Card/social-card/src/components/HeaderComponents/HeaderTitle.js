3580a2d44150cf39b0242784fd4bb49dfcb608eb
ills5771 <ilyayelizarov1@gmail.com>
2019-02-11 13:24:12 -0800

    Added HeaderTitle h3 and timestamp

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import moment from "moment";
+const timeStamp = moment();
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h3>Lambda School</h3>
+      <span>@LambdaSchool - {timeStamp.format("Do MMM")}</span>
+    </div>
+  );
+};
+
+export default HeaderTitle;

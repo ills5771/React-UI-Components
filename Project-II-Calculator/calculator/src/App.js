79a56c2bedcc0d36ca0d441c6b077b4e20e2b9fb
ills5771 <ilyayelizarov1@gmail.com>
2019-02-12 12:42:20 -0800

    added ActionButton component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import NumberButton from "./components/ButtonComponents/NumberButton";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
 
 const App = () => {
   return (
     <div className="app">
       <CalculatorDisplay />
+      <ActionButton text="clear" />
       <NumberButton />
     </div>
   );

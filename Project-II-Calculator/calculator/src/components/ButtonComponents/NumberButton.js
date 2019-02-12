5f66d2474d550a74565d1df9f315a0821477a729
ills5771 <ilyayelizarov1@gmail.com>
2019-02-12 12:27:46 -0800

    Made changes to Numberbutton and CalculatorDisplay

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const NumberButton = props => {
+  return (
+    <div>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button className="red-button">÷</button>
+    </div>
+  );
+};
+export default NumberButton;

a9c7e5ea79e9d16e290be6d567bab422f7ff648a
ills5771 <ilyayelizarov1@gmail.com>
2019-02-12 13:35:17 -0800

    refactored App.js to a class component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,28 +4,33 @@ import NumberButton from "./components/ButtonComponents/NumberButton";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
 import ActionButton from "./components/ButtonComponents/ActionButton";
 
-const App = () => {
-  return (
-    <div className="app">
-      <CalculatorDisplay />
-      <ActionButton text="clear" />
-      <NumberButton buttonStyle="red-button" text="÷" />
-      <NumberButton buttonStyle="number-button" text="7" />
-      <NumberButton buttonStyle="number-button" text="8" />
-      <NumberButton buttonStyle="number-button" text="9" />
-      <NumberButton buttonStyle="red-button" text="x" />
-      <NumberButton buttonStyle="number-button" text="4" />
-      <NumberButton buttonStyle="number-button" text="5" />
-      <NumberButton buttonStyle="number-button" text="6" />
-      <NumberButton buttonStyle="red-button" text="-" />
-      <NumberButton buttonStyle="number-button" text="1" />
-      <NumberButton buttonStyle="number-button" text="2" />
-      <NumberButton buttonStyle="number-button" text="3" />
-      <NumberButton buttonStyle="red-button" text="+" />
-      <ActionButton text="0" />
-      <NumberButton buttonStyle="red-button" text="=" />
-    </div>
-  );
-};
-
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="app">
+        <CalculatorDisplay />
+        <ActionButton text="clear" />
+        <NumberButton buttonStyle="red-button" text="÷" />
+        <NumberButton buttonStyle="number-button" text="7" />
+        <NumberButton buttonStyle="number-button" text="8" />
+        <NumberButton buttonStyle="number-button" text="9" />
+        <NumberButton buttonStyle="red-button" text="x" />
+        <NumberButton buttonStyle="number-button" text="4" />
+        <NumberButton buttonStyle="number-button" text="5" />
+        <NumberButton buttonStyle="number-button" text="6" />
+        <NumberButton buttonStyle="red-button" text="-" />
+        <NumberButton buttonStyle="number-button" text="1" />
+        <NumberButton buttonStyle="number-button" text="2" />
+        <NumberButton buttonStyle="number-button" text="3" />
+        <NumberButton buttonStyle="red-button" text="+" />
+        <ActionButton text="0" />
+        <NumberButton buttonStyle="red-button" text="=" />
+      </div>
+    );
+  }
+}
 export default App;

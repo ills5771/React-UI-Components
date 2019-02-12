6bd94937a4ccfb1b523b48f9774910355c574acb
ills5771 <ilyayelizarov1@gmail.com>
2019-02-11 18:08:49 -0800

    added increment functionality to retweet and favorite icons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,23 +1,52 @@
 import React from "react";
 import "./Footer.css";
 
-const Footer = () => {
-  return (
-    <div className="footer-container">
-      <i class="large material-icons" style={{ opacity: "0.5" }}>
-        chat_bubble_outline
-      </i>
-      <i class="large material-icons" style={{ opacity: "0.5" }}>
-        autorenew
-      </i>
-      <i class="large material-icons" style={{ opacity: "0.5" }}>
-        favorite_border
-      </i>
-      <i class="large material-icons" style={{ opacity: "0.5" }}>
-        mail_outline
-      </i>
-    </div>
-  );
-};
+class Footer extends React.Component {
+  state = {
+    retweets: null,
+    likes: null
+  };
 
+  retweetIncrement = ev => {
+    ev.preventDefault();
+    this.setState({ retweets: this.state.retweets + 1 });
+  };
+  likesIncrement = ev => {
+    ev.preventDefault();
+    this.setState({ likes: this.state.likes + 1 });
+  };
+  render() {
+    return (
+      <div className="footer-container">
+        <i class="large material-icons" style={{ opacity: "0.5" }}>
+          chat_bubble_outline
+        </i>
+        <div>
+          <i
+            class="large material-icons"
+            onClick={this.retweetIncrement}
+            style={{ opacity: "0.5" }}
+          >
+            autorenew
+          </i>
+          <span className="increment">{this.state.retweets}</span>
+        </div>
+        <div>
+          <i
+            class="large material-icons"
+            onClick={this.likesIncrement}
+            onstyle={{ opacity: "0.5" }}
+          >
+            favorite_border
+          </i>
+          <span className="increment">{this.state.likes}</span>
+        </div>
+
+        <i class="large material-icons" style={{ opacity: "0.5" }}>
+          mail_outline
+        </i>
+      </div>
+    );
+  }
+}
 export default Footer;
